5d021a6bc1aeb4e81c359abca9a72bc0075f7c93
AnthonyCheongMQ46600213 <anthony.cheong@students.mq.edu.au>
2021-07-29 12:12:51 +1000

    deleted println bye 2 and 3

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World");
         System.out.println("bye");
-        System.out.println("bye2");
-        //push to different repo
-        System.out.println("bye3");
+  
     }
 }
