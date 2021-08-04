c502d65ddafd1798e80e1302bb4c71d8b87cf96b
AnthonyCheongMQ46600213 <anthony.cheong@students.mq.edu.au>
2021-08-05 03:19:12 +1000

    task 3 coded

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+public class Main extends JFrame{//extends meeaning inheriting properties from a parent class
+    
     public static void main(String[] args) throws Exception {
-     
-        System.out.println("Red vs. Blue");
+     Main window = new Main();
+    // window.run();
+
+    }
+
+public class Canvas extends JPanel {
+    public Canvas() {
+        setPreferredSize(new Dimension(720,720));
+    }
+@Override //to indicate that this method is used over the one from JPanel
+    public void paint(Graphics g){
+        for(int i = 10; i < 710; i+=35) {
+            for(int j = 10; j < 710; j+= 35) {
+                g.setColor(Color.WHITE); //set the color before filling or drawing the rectangle
+                g.fillRect(i,j,35,35);
+                g.setColor(Color.BLACK);
+                g.drawRect(i,j,35,35);
+            }
+        }
+    }
+}
+//Constructor
+    public Main() {//methods under the class Window
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Canvas();
+        setContentPane(canvas);//sets the 'content' of the window with the canvas
+        this.pack();
+        this.setVisible(true);
     }
 }
